9f054562415a69a98206f37ebdfc682c1929e797
Vinicius Pietscher <vini.914@gmail.com>
2022-04-25 21:22:30 -0400

    Lesson 1.5 (#6)
    * add useEffect hook to save todoList on localStorage
    
    * stringfy object saved in localstorage
    
    * getItem from localStorage and parse the json
    
    * move useState and useEffect to custom hook, changed div to fragment
    
    * check if localStorage todoList already exists

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import TodoList from "./TodoList"
 import AddTodoForm from "./AddTodoForm"
 
+function useSemiPersistentState() {
+  const [todoList, setTodoList] = useState(
+    JSON.parse(localStorage.getItem("savedTodoList"))
+      ? JSON.parse(localStorage.getItem("savedTodoList"))
+      : []
+  )
+
+  useEffect(() => {
+    localStorage.setItem("savedTodoList", JSON.stringify(todoList))
+  }, [todoList])
+
+  return [todoList, setTodoList]
+}
+
 function App() {
-  const [todoList, setTodoList] = useState([])
+  const [todoList, setTodoList] = useSemiPersistentState()
 
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo])
   }
 
   return (
-    <div>
+    <>
       <header style={{ textAlign: "center" }}>
         <h1>Todo List</h1>
       </header>
       <hr />
       <AddTodoForm onAddTodo={addTodo} />
       <TodoList todoList={todoList} />
-    </div>
+    </>
   )
 }
 
