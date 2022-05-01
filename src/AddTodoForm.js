d5c1eb4502d08cd1dd9d40498aa131cb747e756b
Vinicius Pietscher <vini.914@gmail.com>
2022-05-01 17:43:21 -0400

    Lesson 1.6 (#7)
    * Create Reusable Input with Label Component
    
    * Refactor Input to use Component Composition
    
    * Add auto-focus to input
    
    * add remove button to list items
    
    * implement @kwilson05 changes - pass id as prop and check if isFocused is passed as prop

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import InputWithLabel from "./InputWithLabel"
 
 function AddTodoForm({ onAddTodo }) {
   const [todoTitle, setTodoTitle] = useState("")
@@ -19,14 +20,14 @@ function AddTodoForm({ onAddTodo }) {
 
   return (
     <form onSubmit={handleAddTodo}>
-      <label htmlFor='todoTitle'>Title: </label>
-      <input
-        type='text'
+      <InputWithLabel
         id='todoTitle'
-        name='title'
-        value={todoTitle}
-        onChange={handleTitleChange}
-      />
+        todoTitle={todoTitle}
+        handleTitleChange={handleTitleChange}
+        isFocused
+      >
+        Title:
+      </InputWithLabel>
       <button type='submit'>Add</button>
     </form>
   )
