30fc0bb41eebc9ca9ccaa768cac38bab0c9d132a
Vinicius Pietscher <vini.914@gmail.com>
2022-05-09 10:56:27 -0400

    Lesson 1.7 (#8)
    * Remove Custom Hook
    
    * Async
    
    * Add Loading State
    
    * Create Conditional Loading Indicator

--- src/App.js
+++ src/App.js
@@ -2,20 +2,31 @@ import { useState, useEffect } from "react"
 import TodoList from "./TodoList"
 import AddTodoForm from "./AddTodoForm"
 
-function useSemiPersistentState() {
-  const [todoList, setTodoList] = useState(
-    JSON.parse(localStorage.getItem("savedTodoList")) || []
-  )
+function App() {
+  const [todoList, setTodoList] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
-    localStorage.setItem("savedTodoList", JSON.stringify(todoList))
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          data: {
+            todoList: JSON.parse(localStorage.getItem("savedTodoList")) || [],
+          },
+        })
+      }, 2000)
+    })
+    promise.then((result) => {
+      setTodoList(result.data.todoList)
+      setIsLoading(false)
+    })
+  }, [])
+
+  useEffect(() => {
+    if (!isLoading) {
+      localStorage.setItem("savedTodoList", JSON.stringify(todoList))
+    }
   }, [todoList])
-
-  return [todoList, setTodoList]
-}
-
-function App() {
-  const [todoList, setTodoList] = useSemiPersistentState()
 
   function handleRemoveTodo(id) {
     const newTodoList = todoList.filter((todoItem) => id !== todoItem.id)
@@ -34,7 +45,11 @@ function App() {
       </header>
       <hr />
       <AddTodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} onRemoveTodo={handleRemoveTodo} />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <TodoList todoList={todoList} onRemoveTodo={handleRemoveTodo} />
+      )}
     </>
   )
 }
